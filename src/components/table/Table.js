ec48f5bab534c872fae48ee9cb8a539a3f082a43
TheMrBlackLord <theblacklord05@gmail.com>
2024-03-11 13:31:01 +0300

    added initial table render logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,39 @@
 'use strict';
 export class Table {
-   constructor(records) {
-      this.records = records;
+   constructor(records, prices) {
+      this.records = this.#getValidRecords(records);
+      this.prices = prices;
    }
+
+   #getValidRecords(records) {
+      return records;
+   }
+
    render(rootElement) {
+      if (!rootElement) {
+         throw new Error('root element is not specified');
+      }
       const tableElement = document.createElement('table');
+      const tBody = document.createElement('tbody');
+      const tHead = document.createElement('thead');
+      tableElement.append(tHead, tBody);
+      tableElement.className = 'table';
+
+      for (const header of ['Компания', ...Object.keys(this.prices)]) {
+         const thEl = document.createElement('th');
+         thEl.innerText = header;
+         tHead.appendChild(thEl);
+      }
+
+      for (const record of this.records) {
+         const rowEl = document.createElement('tr');
+         Object.entries(record).forEach(([key, value]) => {
+            const colEl = document.createElement('td');
+            colEl.innerText = `${key}: ${value}`;
+            rowEl.appendChild(colEl);
+         });
+         tBody.appendChild(rowEl);
+      }
+      rootElement.appendChild(tableElement);
    }
 }
